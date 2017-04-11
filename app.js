c7e680e8e5196ffd8dfbad352c3e816fcfd1a523
Roberto Alicata <roberto.alicata@gmail.com>
2017-04-11 16:06:10 +0200

    Add Static Path and Body Parser

--- app.js
+++ app.js
@@ -4,17 +4,40 @@ var path = require('path');
 
 var app = express();
 
+/*
 var logger = function(req, res, next) {
   console.log('logging...');
   next();
 }
-
 app.use(logger);
+*/
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'public')));
+
+var people = [
+  {
+    name: 'Roberto',
+    age: 42
+  },
+  {
+    name: 'John',
+    age: 22
+  },
+  {
+    name: 'Jane',
+    age: 19
+  }
+];
 
 app.get('/', function(req, res){
-  res.send('Hello World');
+  res.json(people);
 });
 
 app.listen(3000, function() {
   console.log('Server started on port 3000...');
-})
+});
