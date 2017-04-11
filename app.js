863d32c4920829386dd63f5b9c3ba298b01774fa
Roberto Alicata <roberto.alicata@gmail.com>
2017-04-11 12:57:10 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+var logger = function(req, res, next) {
+  console.log('logging...');
+  next();
+}
+
+app.use(logger);
+
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+app.listen(3000, function() {
+  console.log('Server started on port 3000...');
+})
